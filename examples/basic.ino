b2735bb781f927ea3045c98a038037364c8d46cd
S-A-L-M-A-N-A-N-S-A-R-I <100040560+S-A-L-M-A-N-A-N-S-A-R-I@users.noreply.github.com>
2024-07-15 01:16:41 +0530

    Create basic.ino

---
+++ examples/basic.ino
@@ -0,0 +1,31 @@
+#include <softMotor.h>
+#include <elapsedMillis.h>
+
+// Define your pins and settings
+const int motorPin = 9; //Must be PWM pin
+const byte maxSpeed = 255;
+const byte offSpeed = 0;
+const int acceleration = 10;
+
+// Create an instance of softMotor
+softMotor pumpMotor(motorPin, maxSpeed, offSpeed, acceleration);
+
+void setup() {
+    pumpMotor.on(); // Start the motor
+}
+
+void loop() {
+    // Update motor state
+    pumpMotor.alive();
+    
+    // Example: Turn motor on and off every 3 seconds
+    static elapsedMillis timer;
+    if (timer > 5000) {
+        if (pumpMotor.currentspeed() > 0) {
+            pumpMotor.off();
+        } else {
+            pumpMotor.on();
+        }
+        timer = 0;
+    }
+}
